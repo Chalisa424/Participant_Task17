95bd8ea683841fd87fa8a32ee70896b7add84edb
Chalisa424 <chalisa_wongsa@cmu.ac.th>
2025-03-12 22:09:20 +0700

    Task4.9

--- src/App.vue
+++ src/App.vue
@@ -19,10 +19,9 @@ import HelloWorld from './components/HelloWorld.vue'
 
 </template>
 
-<style scoped>
+<style>
 #layout {
  font-family: Avenir, Helvetica, Arial, sans-serif;
- -webkit-font-smoothing: antialiased;
  -moz-osx-font-smoothing: grayscale;
  text-align: center;
  color: #2c3e50;
@@ -36,5 +35,8 @@ nav a {
 }
 nav a.router-link-exact-active {
  color: #42b983;
+}
+h2 {
+  font-size: 20px;
 }
 </style>
