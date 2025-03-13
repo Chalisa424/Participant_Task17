f9ea41f4a26cc511f264757537d5894414ebbedb
Chalisa424 <chalisa_wongsa@cmu.ac.th>
2025-03-14 00:07:13 +0700

    Task7.5

--- src/views/EventListView.vue
+++ src/views/EventListView.vue
@@ -3,47 +3,9 @@ import EventCard from '@/components/EventCard.vue'
 import eventService from '@/services/EventService'
 import { ref } from 'vue'
 import type { Event } from '@/types'
-const events = ref<Event[]>([
-{
-    id: 1,
-    category: 'Category 1',
-    title: 'Event 1',
-    description: 'Description of Event 1',
-    date: '2021-10-10',
-    time: '10:00',
-    location: 'Location 1',
-    petsAllowed: true,
-    organizer: {
-      name: 'Organizer 1',
-    },
-  },
-  {
-    id: 2,
-    category: 'Category 2',
-    title: 'Event 2',
-    description: 'Description of Event 2',
-    date: '2021-10-11',
-    time: '11:00',
-    location: 'Location 2',
-    petsAllowed: false,
-    organizer: {
-      name: 'Organizer 2',
-    },
-  },
-  {
-    id: 3,
-    category: 'Category 3',
-    title: 'Event 3',
-    description: 'Description of Event 3',
-    date: '2021-10-12',
-    time: '12:00',
-    location: 'Location 3',
-    petsAllowed: true,
-    organizer: {
-      name: 'Organizer 3',
-    },
-  },
- ])
+
+const events = ref<Event[]>([])
+
 
  interface EventResponse {
   data: Event[]
