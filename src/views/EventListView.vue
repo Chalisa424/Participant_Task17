68ab1640dbc95727603fe2de1062197ecaab791d
Chalisa424 <chalisa_wongsa@cmu.ac.th>
2025-03-14 02:37:24 +0700

    Task9.3

--- src/views/EventListView.vue
+++ src/views/EventListView.vue
@@ -1,19 +1,25 @@
 <script setup lang="ts">
 import EventCard from '@/components/EventCard.vue'
 import eventService from '@/services/EventService'
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
 import type { Event } from '@/types'
 
 const events = ref<Event[]>([])
 
 
- interface EventResponse {
-  data: Event[]
-}
+//  interface EventResponse {
+//   data: Event[]
+// }
 
-eventService.getEvents().then((response:EventResponse) => {
-  events.value = response.data
-})
+interface Props {
+  page: number
+}
+const props = defineProps<Props>()
+const page = computed(() => props.page)
+
+eventService.getEvents(page.value, 2).then((response) => {
+   events.value = response.data
+ })
 
 </script>
 
