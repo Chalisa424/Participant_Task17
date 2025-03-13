309dd3e067b9ecc3e6684ef1da3f09fd5e7a0d7f
Chalisa424 <chalisa_wongsa@cmu.ac.th>
2025-03-13 23:24:06 +0700

    Task7.4

--- src/views/EventDetailView.vue
+++ src/views/EventDetailView.vue
@@ -1,7 +1,19 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 import type { Event } from '@/types'
+import eventService from '@/services/EventService'
+
 const event = ref<Event>()
+const id = ref<number>(1)
+
+eventService
+  .getEvent(id.value)
+  .then((response) => {
+    event.value = response.data
+  })
+  .catch((error) => {
+    console.error('There was an error!', error)
+  })
 
 </script>
 <template>
