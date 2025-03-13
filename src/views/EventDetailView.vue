f270f8076b4d39a7e1446861d4315f39ddbadaf7
Chalisa424 <chalisa_wongsa@cmu.ac.th>
2025-03-13 23:08:17 +0700

    Task7.3

---
+++ src/views/EventDetailView.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import type { Event } from '@/types'
+const event = ref<Event>()
+</script>
+<template>
+  <div v-if="event">
+    <h1>{{ event.title }}</h1>
+    <p>{{ event.time }} on {{ event.date }} @ {{ event.location }}</p>
+    <p>{{ event.description }}</p>
+  </div>
+  <div v-else>
+    <p>Loading...</p>
+  </div>
+</template>
