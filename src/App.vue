f1947a101b828887de578b10b445efc9f94ae9dd
Chalisa424 <chalisa_wongsa@cmu.ac.th>
2025-03-14 19:43:29 +0700

    Task13.4

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,19 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { useMessageStore } from '@/stores/message'
+import { storeToRefs } from 'pinia'
+
+const store = useMessageStore()
+const { message } = storeToRefs(store)
+
 </script>
 
 <template>
   <div id="layout">
     <header>
+      <div id="flashMessage" v-if="message">
+        <h4>{{ message }}</h4>
+        </div>
 
       <div class="wrapper">
         <nav>
@@ -41,4 +49,17 @@ nav a.router-link-exact-active {
 h2 {
   font-size: 20px;
 }
+
+@keyframes yellofade {
+  from {
+    background-color: yellow;
+  }
+  to {
+    background-color: transparent;
+  }
+}
+#flashMessage {
+  animation: yellofade 3s ease-in-out;
+}
+
 </style>
