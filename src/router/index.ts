355ee03ef5972ce054ea63e9c8a46a0fccf1672b
Chalisa424 <chalisa_wongsa@cmu.ac.th>
2025-03-14 18:22:05 +0700

    Task12.3_Add_Routing

--- src/router/index.ts
+++ src/router/index.ts
@@ -51,6 +51,12 @@ const router = createRouter({
       ],
      },
      {
+      path: '/404/:resource',
+      name: '404-resource-view',
+      component: NotFoundView,
+      props: true,
+    },
+     {
       path: '/:catchAll(.*)',
       name: 'not-found',
       component: NotFoundView,
