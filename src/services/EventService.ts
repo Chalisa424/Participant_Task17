b9aea8a3580b62b43a6acf6457c63fdbdca5bfe7
Chalisa424 <chalisa_wongsa@cmu.ac.th>
2025-03-19 01:56:03 +0700

    Initial Setting From Clone Project

---
+++ src/services/EventService.ts
@@ -0,0 +1,19 @@
+
+import axios from 'axios'
+const apiClient = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
+  withCredentials: false,
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
+  },
+})
+
+export default {
+  getEvents(pageNo: number, pageSize: number) {
+    return apiClient.get('/events?pageNo=' + pageNo + '&pageSize=' + pageSize)
+  },
+  getEvent(id: number) {
+    return apiClient.get(`/events/${id}`)
+  },
+}
---
+++ src/views/event/LayoutView.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { useEventStore } from '@/stores/event'
+import { storeToRefs } from 'pinia'
+
+const store = useEventStore()
+const { event } = storeToRefs(store)
+
+const props = defineProps<{ id: string }>()
+const id = Number(props.id)
+</script>
+
+<template>
+  <div v-if="event">
+    <h1>{{ event.title }}</h1>
+    <nav>
+      <router-link :to="{ name: 'event-detail-view', params: { id } }">Details</router-link>
+      |
+      <router-link :to="{ name: 'event-register-view', params: { id } }">Register</router-link>
+      |
+      <router-link :to="{ name: 'event-edit-view', params: { id } }">Edit</router-link>
+    </nav>
+
+    <router-view :event="event" :id="id" />
+  </div>
+  <div v-else>
+    <p>Loading...</p>
+  </div>
+</template>
