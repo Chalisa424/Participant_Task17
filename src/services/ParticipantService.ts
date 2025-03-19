8bc43e60563f567dcbd188cf0ae20f895d81c1cf
Chalisa424 <chalisa_wongsa@cmu.ac.th>
2025-03-20 00:29:26 +0700

    Add ParticipantService

---
+++ src/services/ParticipantService.ts
@@ -0,0 +1,17 @@
+import axios from "axios";
+const apiClient = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
+  withCredentials: false,
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json"
+  }
+});
+export default {
+  getParticipants(pageNo: number, pageSize: number) {
+    return apiClient.get("/participants?pageNo=" + pageNo + "&pageSize=" + pageSize);
+  },
+  getParticipant(id: number) {
+    return apiClient.get(`/participants/${id}`);
+  }
+};
